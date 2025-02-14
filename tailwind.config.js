d48d92dd727017c9b12486c55796223422f0270a
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2025-02-14 19:05:49 +0700

    pake cdn tailwind ajalah, gak bisa diinstall, dan sedikit mendesign

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './tasks/templates/**/*.html', // Sesuaikan dengan jalur file HTML Anda
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
